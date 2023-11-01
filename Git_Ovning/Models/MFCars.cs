997d301026a82aa9078531d04f9f93dac9a26df7
MagFra <47413425+MagFra@users.noreply.github.com>
2023-11-01 16:35:27 +0100

    Extended MFCars. Added Year and color.

--- Git_Ovning/Models/MFCars.cs
+++ Git_Ovning/Models/MFCars.cs
@@ -1,10 +1,14 @@
-﻿namespace Git_Ovning.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Git_Ovning.Models
 {
     public class MFCars
     {
         public int MFCarsId { get; set; }
         public string Brand { get; set;}
         public string Model { get; set;}
+        [Range(1930, 3000)]
+        public int YearModel { get; set; }
         public string PrimaryColor { get; set; } = "blue";
     }
 }
