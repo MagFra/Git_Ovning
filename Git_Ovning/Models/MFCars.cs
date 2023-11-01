481fd24eb15ce6fdf75aa503222b10214384653c
MagFra <47413425+MagFra@users.noreply.github.com>
2023-11-01 16:16:12 +0100

    Added MFCars

---
+++ Git_Ovning/Models/MFCars.cs
@@ -0,0 +1,9 @@
+﻿namespace Git_Ovning.Models
+{
+    public class MFCars
+    {
+        public int MFCarsId { get; set; }
+        public string Brand { get; set;}
+        public string Model { get; set;}
+    }
+}
