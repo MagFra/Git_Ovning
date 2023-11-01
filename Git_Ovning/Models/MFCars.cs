72db4b19ef92951b90799143d7a9a33f0d8ec3bc
Björn Hansen <dornax@msn.com>
2023-11-01 16:25:34 +0100

    Added color to MFCars

--- Git_Ovning/Models/MFCars.cs
+++ Git_Ovning/Models/MFCars.cs
@@ -1,9 +1,13 @@
-﻿namespace Git_Ovning.Models
+﻿using System.Net.Sockets;
+
+namespace Git_Ovning.Models
 {
     public class MFCars
     {
         public int MFCarsId { get; set; }
         public string Brand { get; set;}
         public string Model { get; set;}
+        public string Color { get; set; }
+
     }
 }
