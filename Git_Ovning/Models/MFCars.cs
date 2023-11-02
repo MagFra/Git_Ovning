d61dbad985dd02c072ddd6125977d8ad5586103f
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-02 10:32:43 +0100

    I classen MFCars satte string.empty på obligatoriska fält och gorde "Pri.Col." till nullable med ett ?

--- Git_Ovning/Models/MFCars.cs
+++ Git_Ovning/Models/MFCars.cs
@@ -4,11 +4,14 @@ namespace Git_Ovning.Models
 {
     public class MFCars
     {
+        [Key]
         public int MFCarsId { get; set; }
-        public string Brand { get; set;}
-        public string Model { get; set;}
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+        [Required]
+        public string Model { get; set; } = string.Empty;
         [Range(1930, 3000)]
-        public int YearModel { get; set; }
-        public string PrimaryColor { get; set; } = "blue";
+        public int YearModel { get; set; }       
+        public string? PrimaryColor { get; set; }
     }
 }
